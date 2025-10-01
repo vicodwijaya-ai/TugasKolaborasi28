6b94891c63bb82d59df2a93cd4ffe57aee736629
ToriqJanuarsyah <toriqjanuarsyah@gmail.com>
2025-10-01 13:00:14 +0700

    Test

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+
+        System.out.println("kopi-kopi apa yang di kepala?");
     }
     
 }
